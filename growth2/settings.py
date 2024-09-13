c14c18bbbe9f464bfe3b476887bf35043e480329
sanderlight <116835755+sanderlight@users.noreply.github.com>
2024-09-13 00:55:17 -0400

    Update settings.py

--- growth2/settings.py
+++ growth2/settings.py
@@ -141,7 +141,9 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = ["static"]
 STATIC_ROOT = "static"
 
-STATICFILES_STORAGE = "whitenoise.storage/CompressedManifestDtaticFilesStorage"
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
